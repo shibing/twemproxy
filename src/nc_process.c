cb432ded78819a5df8474e62185e1f074f54eea3
²®³Ï <yi.fu@alibaba-inc.com>
2014-11-16 21:53:55 +0800

    add process structure

---
+++ src/nc_process.c
@@ -0,0 +1,83 @@
+#include <stdlib.h>
+#include <nc_core.h>
+#include <nc_process.h>
+#include <nc_event.h>
+#include <nc_server.h>
+int nc_current_process_slot = 0;
+
+
+static rstatus_t proxy_each_add_conn(void *elem, void *data)
+{
+    rstatus_t status;
+    struct server_pool *pool = elem;
+    struct conn *p;
+    p = pool->p_conn;
+
+    nc_process_t *process = data;
+
+    status = event_add_conn(process->ep, p);
+    if (status < 0) {
+        log_error("event add conn e %d p %d on addr '%.*s' failed: %s",
+                  process->ep, p->sd, pool->addrstr.len, pool->addrstr.data,
+                  strerror(errno));
+        return NC_ERROR;
+    }
+
+    status = event_del_out(process->ep, p);
+    if (status < 0) {
+        log_error("event del out e %d p %d on addr '%.*s' failed: %s",
+                  process->ep, p->sd, pool->addrstr.len, pool->addrstr.data,
+                  strerror(errno));
+        return NC_ERROR;
+
+    }
+
+    return NC_OK;
+     
+
+}
+
+static void nc_process_init(struct context *ctx,nc_process_t *process){
+    int status, ep;
+    struct epoll_event *event;
+
+    ep = epoll_create(EVENT_SIZE_HINT);
+    if (ep < 0) {
+        log_error("epoll create of size %d failed: %s", EVENT_SIZE_HINT, strerror(errno));
+    }
+
+    event = nc_calloc(EVENT_SIZE_HINT, sizeof(*event));
+    if (event == NULL) {
+        status = close(ep);
+        if (status < 0) {
+            log_error("close e %d failed, ignored: %s", ep, strerror(errno));
+        }
+    }
+
+    process->ep = ep;
+    process->event = event;
+
+    log_debug(LOG_INFO, "e %d ", process->ep);
+
+
+    status = array_each(&ctx->pool, proxy_each_add_conn, process); 
+
+
+
+}
+
+void
+process_loop(struct context *ctx,int process_index)
+{
+    nc_processes[process_index].pid = getpid();
+    nc_current_process_slot = process_index;
+    nc_process_init(ctx,&nc_processes[process_index]);
+
+    log_debug(LOG_VVERB, "spawn process process_id = %d ", getpid());
+    for(;;){
+        core_loop(ctx); 
+    }
+    exit(1);
+}
+
+
---
+++ src/nc_process.h
@@ -0,0 +1,19 @@
+#ifndef _NC_PROCESS_H_
+#define _NC_PROCESS_H_
+typedef struct {        
+    pid_t           pid;
+    int                ep;          /* epoll device */
+    struct epoll_event *event;      /* epoll event */
+    int                 status;
+    int        channel[2];                                
+    void               *data;   
+    char               *name;                                      
+} nc_process_t;
+
+#define NC_MAX_PROCESSES         1024
+nc_process_t    nc_processes[NC_MAX_PROCESSES];
+extern int nc_current_process_slot;
+
+void process_loop(struct context *ctx, int process_index);
+
+#endif
